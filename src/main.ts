90bdb30f56887e0124ead948ecb3c7102c2ecdef
Brendon Fallquist <brendon.fallquist@bfall.me>
2024-08-18 14:17:39 -0400

    _ minor css changes

--- src/main.ts
+++ src/main.ts
@@ -110,6 +110,8 @@ class BookCard extends HTMLElement {
           border-radius: 5px 5px 0 0;
           width: 100%;
           height: 100%;
+
+          object-fit: contain;
         }
         .card:hover {
           box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
